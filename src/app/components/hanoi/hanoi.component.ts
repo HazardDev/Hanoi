9404487362f8239f3f543d882ab6f2f82322f59a
JewsOfHazard <logan.saso2016@gmail.com>
2017-02-24 12:40:37 -0800

    At least it shows now

--- src/app/components/hanoi/hanoi.component.ts
+++ src/app/components/hanoi/hanoi.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
 	//moduleId: module.id,
@@ -8,51 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HanoiComponent {
 
+	constructor(private zone: NgZone){}
+
 	iterations: number = 0;
 	disks: number = 3;
 	A = []
 	B = []
 	C = []
 	seconds: number = 1;
-	doHanoi = function(disks: number, source: Array<number>, destination: Array<number>, spare: Array<number>,){
-		setTimeout(() =>
-		{
-			this.iterations++;
-			if (disks == 1) {
+	doHanoi = function (disks: number, source: Array<number>, using: Array<number>, destination: Array<number>, ) {
+				//console.log(this.iterations);
+				this.iterations++;
+				if (disks == 1) {
+						this.move(source, destination);
+					return;
+				}
+				this.doHanoi(disks - 1, source, destination, using);
 				this.move(source, destination);
-				return;
-			}
-			this.doHanoi(disks - 1, source, spare, destination);
-			this.move(source, destination);
-			this.doHanoi(disks - 1, spare, destination, source);
-		}, this.seconds * 1000);
+				this.doHanoi(disks - 1, using, source, destination);
+
 	}
 
-	move = function(source: Array<number>, destination: Array<number>){
-		setTimeout(()=>{
+	move = function (source: Array<number>, destination: Array<number>) {
+		setTimeout(() => {
 			destination.unshift(source.shift());
-		}, this.seconds * 1000);
+		}, 1000)
 	}
 
-	populate = function(top: number): Array<number>{
-		console.log("Populate Called");
-		let returnMe: Array<number> = []; 
-		for(let i: number = top; i > 0; i--){
+	populate = function (top: number): Array<number> {
+		let returnMe: Array<number> = [];
+		for (let i: number = top; i > 0; i--) {
 			returnMe.push(i);
 		}
 		return returnMe;
 	}
 
-	hanoi = function(){
+	hanoi = function () {
 		console.log("Running Hannoi");
 		this.iterations = 0;
 		this.A = this.populate(this.disks);
 		this.B = []
 		this.C = []
-		this.doHanoi(this.disks, this.A, this.C, this.B);
+		this.doHanoi(this.disks, this.A, this.B, this.C);
 	}
 
-	printArr = function(){
+	printArr = function () {
 		console.log(this.A);
 		console.log(this.B);
 		console.log(this.C);
